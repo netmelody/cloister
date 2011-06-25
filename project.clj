22a917fda30fe6cf98447cdc66b0806d0ed53061
scarytom <t.denley@cantab.net>
2011-06-25 14:26:41 +0100

    initial Leiningen built skeleton with eclipse project sugar.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject org.netmelody/cloister "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/cloister/test/core.clj
@@ -0,0 +1,6 @@
+(ns cloister.test.core
+  (:use [cloister.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
