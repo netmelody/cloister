9d019f37941f590c27b5badfbd7e7724122da5a6
scarytom <t.denley@cantab.net>
2011-06-26 13:59:24 +0100

    generate eclipse project files from leiningen

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject org.netmelody/cloister "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dependencies [
+      [org.clojure/clojure "1.2.1"]
+  ]
+  :dev-dependencies [
+      [lein-clojars "0.6.0"]
+      [lein-eclipse "1.0.0"]
+  ])
