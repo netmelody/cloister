a4d6bd0e6e4c261089ad80d45e00e99b1c27d714
scarytom <t.denley@cantab.net>
2012-07-06 19:33:18 +0100

    thinking about names

--- src/cloister/tokeniser.clj
+++ src/cloister/tokeniser.clj
@@ -3,12 +3,18 @@
 (defn- make-token [type value from to]
   {:type type :value value :from from :to to})
 
+(defn- next-name-from [text]
+  [nil (rest text)])
+
 (defn- next-token-from [text]
   (loop [chars text token-string ""]
     (let [char (first chars) remainder (rest chars)]
       (cond
         (not char) [nil []]
-        (= \space char) (recur remainder token-string)))))
+        (= \space char) (recur remainder token-string)
+        (and (>= \a char) (<= \z char)) (next-name-from chars)
+        (and (>= \A char) (<= \Z char)) (next-name-from chars)
+        ))))
 
 (defn tokenise
   ([text] (tokenise text #{"<" ">" "+" "-" "&"} #{"=" ">" "&" ":"}))
