6ae0a378840b34a18580aa6676c7b76db758deb4
scarytom <t.denley@cantab.net>
2012-07-06 19:25:05 +0100

    walk over spaces

--- src/cloister/tokeniser.clj
+++ src/cloister/tokeniser.clj
@@ -4,13 +4,17 @@
   {:type type :value value :from from :to to})
 
 (defn- next-token-from [text]
-  )
+  (loop [chars text token-string ""]
+    (let [char (first chars) remainder (rest chars)]
+      (cond
+        (not char) [nil []]
+        (= \space char) (recur remainder token-string)))))
 
 (defn tokenise
   ([text] (tokenise text #{"<" ">" "+" "-" "&"} #{"=" ">" "&" ":"}))
   ([text prefixes suffixes]
     (loop [tokens [] content text]
-				    (let [[token remainder] (next-token-from content)]
-				      (if (not token)
-				        tokens
-				        (recur (conj tokens token) remainder))))))
+	    (let [[token remainder] (next-token-from content)]
+	      (if (not token)
+	        tokens
+	        (recur (conj tokens token) remainder))))))
--- test/cloister/test/tokeniser.clj
+++ test/cloister/test/tokeniser.clj
@@ -4,3 +4,9 @@
 
 (deftest tokenises-nil-to-empty-vector
   (is (= [] (cloister.tokeniser/tokenise nil))))
+
+(deftest tokenises-empty-string-to-empty-vector
+  (is (= [] (cloister.tokeniser/tokenise ""))))
+
+(deftest tokenises-blank-string-to-empty-vector
+  (is (= [] (cloister.tokeniser/tokenise "     "))))
