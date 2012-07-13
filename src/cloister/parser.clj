7f16ac191e33ac79452202af2bc466ff9d9fb808
Tom Denley <tom.denley@youdevise.com>
2012-07-13 09:26:41 +0100

    starting to play around with the tdop parser

--- src/cloister/parser.clj
+++ src/cloister/parser.clj
@@ -1,5 +1,29 @@
 (ns cloister.parser)
 
+(defn- error [message] (println message))
+
+(def symbol-table {})
+
+(def symbol-proto
+  {:id nil
+   :value nil
+   :null-denotation (fn [] (error "Undefined."))
+   :left-denotation (fn [left] (error "Missing operator."))
+   :statement-denotation (fn [] (error "Undefined."))
+   :left-binding-power 0
+   :right-binding-power 0
+   })
+
+(defn make-symbol
+  ([id] (make-symbol id 0))
+  ([id binding-power]
+    (if-let [existing-symbol (symbol-table id)]
+      (assoc existing-symbol :left-binding-power (max binding-power (:left-binding-power existing-symbol)))
+      (assoc symbol-proto :id id :value id :left-binding-power binding-power)))
+  )
+
 (defn parse [tokens]
   (println "parsed")
   {})
+
+
