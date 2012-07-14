52d14506d83276307d62dbad36d1512033ecef2a
scarytom <t.denley@cantab.net>
2012-07-14 17:50:43 +0100

    parser foo

--- src/cloister/parser.clj
+++ src/cloister/parser.clj
@@ -2,8 +2,6 @@
 
 (defn- error [message] (println message))
 
-(def symbol-table {})
-
 (def symbol-proto
   {:id nil
    :value nil
@@ -11,16 +9,38 @@
    :left-denotation (fn [left] (error "Missing operator."))
    :statement-denotation (fn [] (error "Undefined."))
    :left-binding-power 0
-   :right-binding-power 0
-   })
+   :right-binding-power 0})
+
+(def definition-proto
+  {:reserved false
+   :null-denotation (fn [] (error "return this?"))
+   :left-denotation nil
+   :statement-denotation nil
+   :left-binding-power 0
+   :scope nil
+   :arity nil})
+
+(def scope-proto
+  {:parent nil
+   :definitions {}})
 
 (defn make-symbol
   ([id] (make-symbol id 0))
-  ([id binding-power]
-    (if-let [existing-symbol (symbol-table id)]
-      (assoc existing-symbol :left-binding-power (max binding-power (:left-binding-power existing-symbol)))
-      (assoc symbol-proto :id id :value id :left-binding-power binding-power)))
-  )
+  ([id binding-power] assoc symbol-proto :id id :value id :left-binding-power binding-power))
+
+(defn register-symbol [table symbol]
+  (let [id (:id symbol)]
+    (if-let [existing-symbol (table id)]
+      (assoc table id (assoc existing-symbol :left-binding-power (max binding-power (:left-binding-power existing-symbol))))
+      (assoc table id symbol))))
+
+(defn scope-define [scope token]
+  (let [name (:value token)
+        defs (:definitions scope)]
+    (if (defs name)
+      (error (str name " already defined"))
+      (let [definition (assoc (merge token definition-proto) :scope scope)]
+        (assoc scope :definitions (assoc defs name definition))))))
 
 (defn parse [tokens]
   (println "parsed")
