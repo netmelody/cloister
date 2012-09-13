0e7cf2ecaf7836658dcd497481386c3e9ee0bd95
scarytom <t.denley@cantab.net>
2012-09-13 23:45:27 +0100

    update to lein 2

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cloister "1.0.0-SNAPSHOT"
-  :description "A JavaScript interpreter written in Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[lein-eclipse "1.0.0"]])
+  :description "Cloister: a JavaScript interpreter written in Clojure"
+  :url "http://netmelody.org/cloister"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
