6e307be356d98dd054e2b9d5a2e39f4111a40b69
scarytom <t.denley@cantab.net>
2012-09-27 23:40:39 +0100

    refactor for clarity

--- src/cloister/parser/symbols.clj
+++ src/cloister/parser/symbols.clj
@@ -25,22 +25,15 @@
                             (let [constant (assoc ((:symbol-table world) id) :arity :literal)]
                               [(reserve world constant) constant]))))
 
-(defn make-offset-infix
-  ([id binding-power offset]
-    (make-offset-infix id binding-power offset (fn [world token left]
-                                          (let [[new-world expr] (extract-expression world (- binding-power offset))
-                                                infix (assoc ((:symbol-table world) id) :first left :second expr :arity :binary)]
-                                            [new-world infix]))))
-  ([id binding-power offset left-denotation]
-    (assoc (make-symbol id binding-power) :left-denotation left-denotation)))
+(defn make-offset-infix [id binding-power offset]
+  (assoc (make-symbol id binding-power)
+         :left-denotation (fn [world token left]
+                            (let [[new-world expr] (extract-expression world (- binding-power offset))
+                                  infix (assoc ((:symbol-table world) id) :first left :second expr :arity :binary)]
+                              [new-world infix]))))
 
-(defn make-infix
-  ([id binding-power] (make-offset-infix id binding-power 0))
-  ([id binding-power left-denotation] (make-offset-infix id binding-power 0 left-denotation)))
-
-(defn make-infixr
-  ([id binding-power] (make-offset-infix id binding-power -1))
-  ([id binding-power left-denotation] (make-offset-infix id binding-power -1 left-denotation)))
+(defn make-infix [id binding-power] (make-offset-infix id binding-power 0))
+(defn make-infixr [id binding-power] (make-offset-infix id binding-power -1))
 
 (defn make-assignment [id]
   (assoc (make-symbol id 10) :left-denotation (fn [world token left]
@@ -53,9 +46,6 @@
                                                 prefix (assoc ((:symbol-table world) id) :first expr :arity :unary)]
                                             [(reserve new-world prefix) prefix]))))
   ([id nud] (assoc (make-symbol id) :null-denotation nud)))
-
-(defn make-statement [id statement-denotation]
-  (assoc (make-symbol id) :statement-denotation statement-denotation))
 
 (defn register-symbol [table symbol]
   (let [id (:id symbol)]
@@ -89,23 +79,24 @@
                               [(reserve world this) this]))))
 
 (def ^{:private true} _?
-  (make-infix "?" 20
-              (fn [world token left]
-                (let [[w1 expr1] (extract-expression world 0)
-                      [w2 expr2] (extract-expression (advance w1 ":") 0)]
-                  [w2 (assoc token :first left :second expr1 :third expr2 :arity :ternary)]))))
+  (assoc (make-infix "?" 20)
+         :left-denotation (fn [world token left]
+                            (let [[w1 expr1] (extract-expression world 0)
+                                  [w2 expr2] (extract-expression (advance w1 ":") 0)]
+                              [w2 (assoc token :first left :second expr1 :third expr2 :arity :ternary)]))))
 
 (def ^{:private true} _var
-  (make-statement "var" (fn [world token]
-                          (loop [w world assignments []]
-                            (let [name-token (:token w)]
-                              (if (not (= :name (:arity name-token)))
-                                (report-error name-token "expected a new variable name.")
-                                (let [[w2 assignment] (extract-assignment (reserve (advance w) name-token) name-token)
-                                      a2 (if assignment (conj assignments assignment) assignments)]
-                                  (if (= "," (:id (:token w2)))
-                                    (recur (advance w2 ",") a2)
-                                    [(advance w2 ";") (one-or-many a2)]))))))))
+  (assoc (make-symbol "var")
+         :statement-denotation (fn [world token]
+                                 (loop [w world assignments []]
+                                   (let [name-token (:token w)]
+                                     (if (not (= :name (:arity name-token)))
+                                       (report-error name-token "expected a new variable name.")
+                                       (let [[w2 assignment] (extract-assignment (reserve (advance w) name-token) name-token)
+                                             a2 (if assignment (conj assignments assignment) assignments)]
+                                         (if (= "," (:id (:token w2)))
+                                           (recur (advance w2 ",") a2)
+                                           [(advance w2 ";") (one-or-many a2)]))))))))
 
 (def base-symbol-table (-> {}
                          (register-symbol (make-symbol :end))
