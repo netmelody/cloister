225bb4e6f7d1fc1ff193222b47133037ecdd504c
scarytom <t.denley@cantab.net>
2012-09-29 10:38:32 +0100

    symbols for "return" "{" and "("

--- src/cloister/parser/symbols.clj
+++ src/cloister/parser/symbols.clj
@@ -8,9 +8,9 @@
 (def symbol-proto
   {:id nil
    :value nil
-   :null-denotation (fn [world token] (report-error (:token world) "Undefined.") [world nil])
-   :left-denotation (fn [world token left] (report-error (:token world) "Missing operator.") [world nil])
-   ;:statement-denotation (fn [world token] (report-error (:token world) "Undefined.") [world nil])
+   :null-denotation (fn [world identity-token] (report-error identity-token "Undefined.") [world nil])
+   :left-denotation (fn [world identity-token left] (report-error identity-token "Missing operator.") [world nil])
+   ;:statement-denotation (fn [world identity-token] (report-error (:token world) "Undefined.") [world nil])
    :left-binding-power 0
    :right-binding-power 0})
 
@@ -21,13 +21,13 @@
 (defn make-constant [id value]
   (assoc (make-symbol id)
          :value value
-         :null-denotation (fn [world token]
+         :null-denotation (fn [world identity-token]
                             (let [constant (assoc ((:symbol-table world) id) :arity :literal)]
                               [(reserve world constant) constant]))))
 
 (defn make-offset-infix [id binding-power offset]
   (assoc (make-symbol id binding-power)
-         :left-denotation (fn [world token left]
+         :left-denotation (fn [world identity-token left]
                             (let [[new-world expr] (extract-expression world (- binding-power offset))
                                   infix (assoc ((:symbol-table world) id) :first left :second expr :arity :binary)]
                               [new-world infix]))))
@@ -36,13 +36,13 @@
 (defn make-infixr [id binding-power] (make-offset-infix id binding-power -1))
 
 (defn make-assignment [id]
-  (assoc (make-symbol id 10) :left-denotation (fn [world token left]
+  (assoc (make-symbol id 10) :left-denotation (fn [world identity-token left]
                                                 (let [[new-world expr] (extract-expression world 9)
                                                       infix (assoc ((:symbol-table world) id) :first left :second expr :arity :binary :assignment true)]
                                                   [new-world infix]))))
 
 (defn make-prefix [id]
-  (assoc (make-symbol id) :null-denotation (fn [world token]
+  (assoc (make-symbol id) :null-denotation (fn [world identity-token]
                                              (let [[new-world expr] (extract-expression world 70)
                                                    prefix (assoc ((:symbol-table world) id) :first expr :arity :unary)]
                                                [(reserve new-world prefix) prefix]))))
@@ -70,24 +70,38 @@
 
 (def ^{:private true} _literal
   (assoc (make-symbol :literal)
-         :null-denotation (fn [world token] [world token])))
+         :null-denotation (fn [world identity-token] [world identity-token])))
 
 (def ^{:private true} _this
   (assoc (make-symbol "this")
-         :null-denotation (fn [world token]
-                            (let [this (assoc token :arity :this)]
+         :null-denotation (fn [world identity-token]
+                            (let [this (assoc identity-token :arity :this)]
                               [(reserve world this) this]))))
 
+(def ^{:private true} _open-parens
+  (assoc (make-symbol "(" 80)
+         :left-denotation (fn [world identity-token left]
+                            (report-error identity-token "led not yet implemented!"))
+         :null-denotation (fn [world identity-token]
+                            (let [[w1 expr] (extract-expression world 0)] [(advance w1 ")" expr)]))))
+
+(def ^{:private true} _open-curly
+  (assoc (make-symbol "{")
+         :null-denotation (fn [world identity-token]
+                            (report-error identity-token "nud not yet implemented!"))
+         :statement-denotation (fn [world identity-token]
+                                 (report-error identity-token "std not yet implemented!"))))
+
 (def ^{:private true} _?
-  (assoc (make-infix "?" 20)
-         :left-denotation (fn [world token left]
+  (assoc (make-symbol "?" 20)
+         :left-denotation (fn [world identity-token left]
                             (let [[w1 expr1] (extract-expression world 0)
                                   [w2 expr2] (extract-expression (advance w1 ":") 0)]
-                              [w2 (assoc token :first left :second expr1 :third expr2 :arity :ternary)]))))
+                              [w2 (assoc identity-token :first left :second expr1 :third expr2 :arity :ternary)]))))
 
 (def ^{:private true} _var
   (assoc (make-symbol "var")
-         :statement-denotation (fn [world token]
+         :statement-denotation (fn [world identity-token]
                                  (loop [w world assignments []]
                                    (let [name-token (:token w)]
                                      (if (not (= :name (:arity name-token)))
@@ -98,17 +112,27 @@
                                            (recur (advance w2 ",") a2)
                                            [(advance w2 ";") (one-or-many a2)]))))))))
 
+(def ^{:private true} _return
+  (assoc (make-symbol "return")
+         :statement-denotation (fn [world identity-token]
+                                 (let [[w1 first] (if (not (= ";" (:id (:token world)))) (extract-expression world 0) [world nil])
+                                       this (if (nil? first) identity-token (assoc identity-token :first first))
+                                       w2 (advance w1 ";")]
+                                   (if (not (= "}" (:id (:token w2))))
+                                     (report-error (:token w2) "Unreachable statement"))
+                                   [w2 (assoc this :arity :statement)]))))
+
 (def ^{:private true} _function
-  (assoc (make-prefix "function")
-         :null-denotation (fn [world token]
-                            (let [w1 (assoc world :scope (scope-create-child (:scope world)))
-                                 ; [t w2] (if (= :name (:arity token)) [(assoc token :name (:value token)) (advance (reserve w1 token))] [token w1])
-                                  [t w2] [token w1]
+  (assoc (make-symbol "function")
+         :null-denotation (fn [world identity-token]
+                            (let [token (:token world)
+                                  w1 (assoc world :scope (scope-create-child (:scope world)))
+                                  [this w2] (if (= :name (:arity token)) [(assoc identity-token :name (:value token)) (advance (reserve w1 token))] [identity-token w1])
                                   w3 (advance w2 "(")]
                               ; TODO: parse args here
                               (let [[w4 statements] (extract-statements (advance (advance w3 ")") "{"))
                                     w5 (advance w4 "}")]
-                                [(assoc w5 :scope (:parent (:scope w5))) (assoc t :first [] :arity :function :second statements)])))))
+                                [(assoc w5 :scope (:parent (:scope w5))) (assoc this :first [] :arity :function :second statements)])))))
 
 (def base-symbol-table (-> {}
                          (register-symbol (make-symbol :end))
@@ -132,5 +156,8 @@
                          (register-symbol (make-assignment "+="))
                          (register-symbol (make-assignment "-="))
                          (register-symbol _?)
+                         (register-symbol _open-parens)
+                         (register-symbol _open-curly)
                          (register-symbol _var)
+                         (register-symbol _return)
                          (register-symbol _function)))
